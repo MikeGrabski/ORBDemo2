bee904a499f7e58a55ed9b02e9dc9268a2c08c45
iosuser11 <matchkalyananahit@gmail.com>
2016-07-22 11:56:38 +0400

    added methods to camerapreview class

--- app/src/main/java/com/example/iosuser12/orbtest/PicturePreview.java
+++ app/src/main/java/com/example/iosuser12/orbtest/PicturePreview.java
@@ -2,7 +2,17 @@ package com.example.iosuser12.orbtest;
 
 import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.util.List;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -10,29 +20,192 @@ import javax.microedition.khronos.opengles.GL10;
 /**
  * Created by iosuser11 on 7/21/16.
  */
-public class PicturePreview extends GLSurfaceView implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener
+public class PicturePreview extends GLSurfaceView implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener, Camera.PreviewCallback
 {
+    private final String vss =
+            "attribute vec2 vPosition;\n" +
+                    "attribute vec2 vTexCoord;\n" +
+                    "varying vec2 texCoord;\n" +
+                    "void main() {\n" +
+                    "  texCoord = vTexCoord;\n" +
+                    "  gl_Position = vec4 ( vPosition.x, vPosition.y, 0.0, 1.0 );\n" +
+                    "}";
+
+    private final String fss =
+            "#extension GL_OES_EGL_image_external : require\n" +
+                    "precision mediump float;\n" +
+                    "uniform samplerExternalOES sTexture;\n" +
+                    "varying vec2 texCoord;\n" +
+                    "void main() {\n" +
+                    "  gl_FragColor = texture2D(sTexture,texCoord);\n" +
+                    "}";
+
+    private int[] hTex;
+    private FloatBuffer pVertex;
+    private FloatBuffer pTexCoord;
+    private int hProgram;
+
+    private byte[] data;
+
+    private Camera mCamera;
+    private SurfaceTexture mSTexture;
+    private ByteBuffer mFullQuadVertices;
+    private boolean updateTexture = false;
+
     public PicturePreview(Context context) {
         super(context);
+
+//        final byte FULL_QUAD_COORDS[] = {-1, 1, -1, -1, 1, 1, 1, -1};
+//        mFullQuadVertices = ByteBuffer.allocateDirect(4 * 2);
+//        mFullQuadVertices.put(FULL_QUAD_COORDS).position(0);
+//        setPreserveEGLContextOnPause(true);
+
+        float[] vtmp = { 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f };
+        float[] ttmp = { 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f };
+        pVertex = ByteBuffer.allocateDirect(8*4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+        pVertex.put ( vtmp );
+        pVertex.position(0);
+        pTexCoord = ByteBuffer.allocateDirect(8*4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+        pTexCoord.put ( ttmp );
+        pTexCoord.position(0);
+
+        setEGLContextClientVersion(2);
+        setRenderer(this);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        mCamera = Camera.open();
+        try {
+            mCamera.setPreviewTexture(mSTexture);
+        } catch ( IOException ioe ) {
+        }
     }
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        hTex = new int[1];
+        GLES20.glGenTextures ( 1, hTex, 0 );
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, hTex[0]);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
 
+        mSTexture = new SurfaceTexture(hTex[0]);
+        mSTexture.setOnFrameAvailableListener(this);
+
+
+
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        hProgram = loadShader ( vss, fss );
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        Camera.Parameters param = mCamera.getParameters();
+        param.set("orientation", "landscape");
+        mCamera.setParameters ( param );
+        mCamera.startPreview();
 
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
+        synchronized(this) {
+            if ( updateTexture ) {
+                mSTexture.updateTexImage();
+                updateTexture = false;
+            }
+        }
+
+        GLES20.glUseProgram(hProgram);
+
+        int ph = GLES20.glGetAttribLocation(hProgram, "vPosition");
+        int tch = GLES20.glGetAttribLocation ( hProgram, "vTexCoord" );
+        int th = GLES20.glGetUniformLocation ( hProgram, "sTexture" );
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, hTex[0]);
+        GLES20.glUniform1i(th, 0);
+
+        GLES20.glVertexAttribPointer(ph, 2, GLES20.GL_FLOAT, false, 4*2, pVertex);
+        GLES20.glVertexAttribPointer(tch, 2, GLES20.GL_FLOAT, false, 4*2, pTexCoord );
+        GLES20.glEnableVertexAttribArray(ph);
+        GLES20.glEnableVertexAttribArray(tch);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        GLES20.glFlush();
     }
 
     @Override
     public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+        updateTexture = true;
+        requestRender();
 
+    }
+
+    private static int loadShader ( String vss, String fss ) {
+        int vshader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+        GLES20.glShaderSource(vshader, vss);
+        GLES20.glCompileShader(vshader);
+        int[] compiled = new int[1];
+        GLES20.glGetShaderiv(vshader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+        if (compiled[0] == 0) {
+            Log.e("Shader", "Could not compile vshader");
+            Log.v("Shader", "Could not compile vshader:"+GLES20.glGetShaderInfoLog(vshader));
+            GLES20.glDeleteShader(vshader);
+            vshader = 0;
+        }
+
+        int fshader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+        GLES20.glShaderSource(fshader, fss);
+        GLES20.glCompileShader(fshader);
+        GLES20.glGetShaderiv(fshader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+        if (compiled[0] == 0) {
+            Log.e("Shader", "Could not compile fshader");
+            Log.v("Shader", "Could not compile fshader:"+GLES20.glGetShaderInfoLog(fshader));
+            GLES20.glDeleteShader(fshader);
+            fshader = 0;
+        }
+
+        int program = GLES20.glCreateProgram();
+        GLES20.glAttachShader(program, vshader);
+        GLES20.glAttachShader(program, fshader);
+        GLES20.glLinkProgram(program);
+
+        return program;
+    }
+
+    byte[] getCurrentFrame() {
+        return data;
+    }
+
+    int getPreviewFormat() {
+        return mCamera.getParameters().getPreviewFormat();
+    }
+
+    int getPreviewHeight() { return mCamera.getParameters().getPreviewSize().height; }
+
+    int getPreviewWidth() {
+        return mCamera.getParameters().getPreviewSize().width;
+    }
+
+    List<Integer> getSupportedPreiewFormats(){
+        return mCamera.getParameters().getSupportedPreviewFormats();
+    }
+
+    void setPreviewFormat(int previewFormat){
+        Camera.Parameters params = mCamera.getParameters();
+        params.setPreviewFormat(previewFormat);
+        mCamera.setParameters(params);
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+        data = bytes;
     }
 }
